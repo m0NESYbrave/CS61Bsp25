1f655564ef922c28fcddfd2e34aacb081b373780
Curry <wobuzhidaoei1014@163.com>
2025-06-15 21:01:36 +0800

    lab02: Completed BombMain.java

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -11,13 +11,18 @@ public class BombMain {
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+//            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+//            b.phase1(null); // Figure this out too
+            IntList L = IntList.of(0, 9, 3, 0, 8);
+            b.phase1(L);
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+//            b.phase2("Figure this out. I wonder where the phases are defined...");
+            // the 1337th item in numbers, so the condition is numbers.size() == 1338
+            b.phase2("793227803");
         }
     }
 }
